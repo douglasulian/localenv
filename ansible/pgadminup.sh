18e38f74038f7c858ad815f7947f28059c9fb8dd
Douglas <douglasulian@hotmail.com>
2017-10-13 20:29:53 -0300

    Moved pgadmin config to project repos, modified pgadmin mount to project repos

--- ansible/pgadminup.sh
+++ ansible/pgadminup.sh
@@ -1,4 +1,4 @@
 #ansible-playbook pgadminup.yaml --ask-become-pass 
-sudo docker run -d -p 5050:5050 -v /home/douglas/repos/localenv/pgadmin/data:/data chorss/docker-pgadmin4
+sudo docker run -d -p 5050:5050 -v /home/douglas/repos/user-clustering-gzh/r-project/data:/data chorss/docker-pgadmin4
 
 
